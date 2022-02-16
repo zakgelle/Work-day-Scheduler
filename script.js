2f7ce1bc8ab0502adb90538b88894ba9e44a3425
zakaria gelle <zak.gelle@gmail.com>
2022-02-16 14:34:25 +0000

    modified script

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function () {
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
+
+      localStorage.setItem(time, text);
+    });
   
 
 
